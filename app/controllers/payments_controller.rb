33744b87a32dae9d120efe4129ca2dfcd1bab936
Luke <lukepierson@hotmail.com>
2019-11-07 11:27:48 +1100

    stripe payments now  add to the current users balance :))

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,29 +1,31 @@
 class PaymentsController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:webhook]
-
-    def success
-    end 
+    before_action :authenticate_user!, except: [:webhook]
 
     def webhook
         p "*******************************"
-        p params
+        p current_user
         payment_id = params[:data][:object][:payment_intent]
         payment = Stripe::PaymentIntent.retrieve(payment_id)
         user_id = payment.metadata.user_id
-        @amount = params[:payment][:data][:object][:display_items][0][:amount]
+        user = User.find(user_id)
+        @amount = params[:payment][:data][:object][:display_items][0][:amount].to_i * 100
+        @amount += user.balance.amount  
+        user.balance.update_attributes(amount: @amount)
     end 
 
     def new  
     end
     
     def confirm
+        amount = params[:amount].to_i * 100 
         session = Stripe::Checkout::Session.create(
             payment_method_types: ["card"], 
             customer_email: current_user.email,
             line_items: [
                 {
                     name: "currency",
-                    amount: params[:amount], 
+                    amount: amount, 
                     currency: "aud", 
                     quantity: 1  
                 }
@@ -40,6 +42,10 @@ class PaymentsController < ApplicationController
         @session_id = session.id 
         @public_key = Rails.application.credentials.dig(:stripe, :public_key)
         # render "confirm"
+    end 
+
+    def success
+
     end 
 
   
